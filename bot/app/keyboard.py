f4ad4505d93419de1407134f747aa181e8b8eeee
Ilya <iloa.spaski123321@gmail.com>
2024-11-14 02:33:11 +0300

    Delete bot/app directory

--- bot/app/keyboard.py
+++
@@ -1,5 +0,0 @@
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-
-url="https://b117-82-146-252-31.ngrok-free.app/index.html"
-builder = InlineKeyboardBuilder()
-builder.button(text="Подать заявку", url=url)
