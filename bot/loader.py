57abe423e19d44a5fbc3ea306b2f35b68c17521b
Ilya <iloa.spaski123321@gmail.com>
2024-11-14 02:32:32 +0300

    Delete bot/loader.py

--- bot/loader.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot, Dispatcher
-from watchdog.observers import Observer
-from config_data import config
-
-bot = Bot(token=config.BOT_TOKEN)
-
-dp = Dispatcher()
-observer = Observer()
